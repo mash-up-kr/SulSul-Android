fbf0f51275474e8aecb21412185f2dc1e5d1b925
SEO-J17 <qw4643987@naver.com>
2023-05-02 18:35:40 +0900

    chore: 도메인 레이어 gradle.kts로 변경

---
+++ domain/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("kotlin")
+    id("kotlin-kapt")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(Google.hiltCore)
+    kapt(Google.hiltCompiler)
+}
