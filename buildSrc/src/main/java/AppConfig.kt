9ee871efbe1a76e5f58fb86a3408655de838fdc8
SEO-J17 <qw4643987@naver.com>
2023-05-02 18:26:51 +0900

    feat: AppConfig에 관한 버전 설정

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,18 @@
+object AppConfig {
+    const val compileSdk = 33
+    const val minSdk = 26
+    const val targetSdk = 33
+    const val kotlinCompilerExt = "1.4.4"
+
+    const val applicationId = "com.mashup.alcoholfree"
+    const val appNamespace = "com.mashup.alcoholfree"
+
+    const val appVersionCode = 1
+    const val appVersionName = "1.0"
+
+    const val dataNamespace = "com.mashup.alcoholfree.data"
+    const val domainNamespace = "com.mashup.alcoholfree.domain"
+    const val presentationNamespace = "com.mashup.alcoholfree.presentation"
+
+    const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
+}
