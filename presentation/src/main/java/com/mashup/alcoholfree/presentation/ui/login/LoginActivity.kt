dc88c10eed582fb7485c9324275712bc7acb73fa
seojeongwoo <qw4643987@naver.com>
2023-05-29 19:57:43 +0900

    feat: 로그인 액티비티 구현

---
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/login/LoginActivity.kt
@@ -0,0 +1,82 @@
+package com.mashup.alcoholfree.presentation.ui.login
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.user.UserApiClient
+import com.mashup.alcoholfree.presentation.ui.home.HomeActivity
+import com.mashup.alcoholfree.presentation.ui.theme.AlcoholFreeAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class LoginActivity : ComponentActivity() {
+    private val viewModel: LoginViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            AlcoholFreeAndroidTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background,
+                ) {
+                    LoginScreen(
+                        onKakaoLoginClick = { kakaoLoginClick() },
+                    )
+                }
+            }
+        }
+    }
+
+    private fun kakaoLoginClick() {
+        with(UserApiClient.instance) {
+            if (isKakaoTalkLoginAvailable(this@LoginActivity)) {
+                loginWithKakaoTalk(this@LoginActivity) { token, error ->
+                    if (error != null) {
+                        // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
+                        // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
+                        if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
+                            return@loginWithKakaoTalk
+                        }
+                        // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인 시도
+                        kakaoAccountLogin()
+                    } else if (token != null) {
+                        viewModel.addKakaoToken(token)
+                    }
+                }
+            } else {
+                kakaoAccountLogin()
+            }
+        }
+    }
+
+    private fun kakaoAccountLogin() {
+        UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
+            if (error != null) {
+                Toast.makeText(
+                    this,
+                    " 로그인 실패",
+                    Toast.LENGTH_SHORT,
+                ).show()
+            } else if (token != null) {
+                Toast.makeText(
+                    this,
+                    " 로그인 성공",
+                    Toast.LENGTH_SHORT,
+                ).show()
+                viewModel.addKakaoToken(token)
+                startActivity(Intent(this, HomeActivity::class.java))
+            }
+        }
+    }
+}
