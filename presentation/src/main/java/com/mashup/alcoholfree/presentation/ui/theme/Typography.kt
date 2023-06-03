7b178772e059511711067d28681dbd92831783b1
AnKyungMoo <sodp2@naver.com>
2023-06-03 17:24:34 +0900

    feat: #12 타이포그라피 정의

---
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/theme/Typography.kt
@@ -0,0 +1,77 @@
+package com.mashup.alcoholfree.presentation.ui.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val H1 = TextStyle(
+    fontSize = 28.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = 42.sp,
+)
+
+val H2 = TextStyle(
+    fontSize = 24.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = 36.sp,
+)
+
+val H3 = TextStyle(
+    fontSize = 20.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = 30.sp,
+)
+
+val H4 = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = 27.sp,
+)
+
+val H5 = TextStyle(
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Bold,
+    lineHeight = 24.sp,
+)
+
+val SubTitle1 = TextStyle(
+    fontSize = 18.sp,
+    fontWeight = FontWeight.Medium,
+    lineHeight = 28.sp,
+)
+
+val SubTitle2 = TextStyle(
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Medium,
+    lineHeight = 24.sp,
+)
+
+val SubTitle3 = TextStyle(
+    fontSize = 14.sp,
+    fontWeight = FontWeight.Medium,
+    lineHeight = 22.sp,
+)
+
+val SubTitle4 = TextStyle(
+    fontSize = 12.sp,
+    fontWeight = FontWeight.Medium,
+    lineHeight = 18.sp,
+)
+
+val ParagraphLg = TextStyle(
+    fontSize = 16.sp,
+    fontWeight = FontWeight.Normal,
+    lineHeight = 24.sp,
+)
+
+val Paragraph = TextStyle(
+    fontSize = 14.sp,
+    fontWeight = FontWeight.Normal,
+    lineHeight = 22.sp,
+)
+
+val ParagraphSm = TextStyle(
+    fontSize = 12.sp,
+    fontWeight = FontWeight.Normal,
+    lineHeight = 18.sp,
+)
