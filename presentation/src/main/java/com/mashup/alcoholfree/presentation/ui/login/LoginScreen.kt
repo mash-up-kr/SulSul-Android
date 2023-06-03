8b099e6ce2e0896426f0f6a3ad6b2f8bc1b76ef2
seojeongwoo <qw4643987@naver.com>
2023-06-03 17:27:49 +0900

    refactor: 버튼 컴포저블로 분리, 컴포즈 프리뷰 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/login/LoginScreen.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/login/LoginScreen.kt
@@ -15,39 +15,52 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.alcoholfree.presentation.R
+import com.mashup.alcoholfree.presentation.ui.theme.AlcoholFreeAndroidTheme
 import com.mashup.alcoholfree.presentation.ui.theme.KakaoContainer
 import com.mashup.alcoholfree.presentation.ui.theme.KakaoSymbol
 
 @Composable
 fun LoginScreen(
-    modifier: Modifier = Modifier,
     onKakaoLoginClick: () -> Unit,
 ) {
     Column(
-        modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Bottom,
     ) {
-        Button(
-            shape = RoundedCornerShape(6.dp),
-            onClick = onKakaoLoginClick,
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp),
-            colors = ButtonDefaults.buttonColors(backgroundColor = KakaoContainer),
-        ) {
-            Icon(
-                painter = painterResource(id = R.drawable.kakao_symbol),
-                contentDescription = null,
-                modifier = Modifier.size(16.dp),
-            )
-            Text(
-                text = stringResource(R.string.kakao_login_label),
-                modifier = Modifier.padding(start = 5.dp),
-                color = KakaoSymbol,
-            )
-        }
+        KakaoLoginButton(onKakaoLoginClick)
+    }
+}
+
+@Composable
+fun KakaoLoginButton(onKakaoLoginClick: () -> Unit) {
+    Button(
+        shape = RoundedCornerShape(6.dp),
+        onClick = onKakaoLoginClick,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(5.dp),
+        colors = ButtonDefaults.buttonColors(backgroundColor = KakaoContainer),
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.kakao_symbol),
+            contentDescription = null,
+            modifier = Modifier.size(16.dp),
+        )
+        Text(
+            text = stringResource(R.string.kakao_login_label),
+            modifier = Modifier.padding(start = 5.dp),
+            color = KakaoSymbol,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    AlcoholFreeAndroidTheme {
+        LoginScreen(onKakaoLoginClick = {})
     }
 }
