e7f87bade33c924a45c7cc931d3e0c6fa699bf1a
AnKyungMoo <sodp2@naver.com>
2023-06-05 00:01:00 +0900

    feat: #17 디버그용 브릿지 페이지 생성

---
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/debug_bridge/DebugBridgeActivity.kt
@@ -0,0 +1,20 @@
+package com.mashup.alcoholfree.presentation.debug_bridge
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.mashup.alcoholfree.presentation.MainActivity
+
+class DebugBridgeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            DebugBridgeScreen(
+                items = listOf(
+                    MainActivity::class.java
+                )
+            )
+        }
+    }
+}
