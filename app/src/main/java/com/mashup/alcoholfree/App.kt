efea965c925ddc31388ec44d500bcdf1ab3551de
SEO JEONGWOO <59912150+SEO-J17@users.noreply.github.com>
2023-06-05 17:16:04 +0900

    #4 카카오 로그인 기능 구현 (#10)
    * chore: 매니페스트에 액티비티 추가 및 카카오 리다이렉션 추가
    
    * feat: 어플리케이션에 카카오 sdk init 코드 추가
    
    * chore: 카카오 로그인 의존성 추가, local.properties의 키값 추가
    
    * chore: 카카오 로그인 의존성 추가, local.properties의 키값 추가
    
    * feat: 카카오 로그인 버튼에 사용할 컬러 추가
    
    * style: 필요없는 main 액티비티 삭제
    
    * feat: 홈 액티비티 구현함
    
    * feat: 홈 뷰모델 구현
    
    * feat: 로그인 액티비티 구현
    
    * feat: 로그인 뷰모델 구현
    
    * chore: 카카오 url 추가
    
    * style: 사용할 string값 strings.xml애 추가
    
    * feat: 카카오 의존성 관련 버전 추가
    
    * #6 웹뷰 틀 만들기 (#9)
    
    * feat: accompanist-webview 의존성 추가
    
    * feat: 공통 웹뷰 액티비티 정의
    
    * feat: 인터넷 퍼미션 추가
    
    * feat: 웹뷰 state를 외부에서 받도록 수정
    
    * feat: javaScript 인터페이스가 있는 웹뷰 정의
    
    * style: 네이밍 수정 (CommonWebView -> SulSulWebView)
    
    * style: 웹뷰 액티비티 제거
    
    * style: 카카오 심볼 drawable 추가
    
    * feat: #11 컬러 팔레트 추가 (#14)
    
    * style: 홈 화면 삭제
    
    * chore: 안쓰이는 의존성 삭제
    
    * style: 안 쓰이는 버전 삭제
    
    * style: 안 쓰이는 import 삭제
    
    * refactor: 버튼 컴포저블로 분리, 컴포즈 프리뷰 추가
    
    * style: 매니페스트 홈 액티비티 코드 제거함
    
    * feat: #12 타이포그라피 정의 (#15)
    
    * feat: 카카오 키해시 가져오는 함수 추가
    
    * chore: 매니페스트에 액티비티 추가 및 카카오 리다이렉션 추가
    
    * feat: 어플리케이션에 카카오 sdk init 코드 추가
    
    * chore: 카카오 로그인 의존성 추가, local.properties의 키값 추가
    
    * chore: 카카오 로그인 의존성 추가, local.properties의 키값 추가
    
    * style: 필요없는 main 액티비티 삭제
    
    * feat: 홈 액티비티 구현함
    
    * feat: 홈 뷰모델 구현
    
    * feat: 로그인 액티비티 구현
    
    * feat: 로그인 뷰모델 구현
    
    * chore: 카카오 url 추가
    
    * style: 사용할 string값 strings.xml애 추가
    
    * feat: 카카오 의존성 관련 버전 추가
    
    * style: 카카오 심볼 drawable 추가
    
    * style: 홈 화면 삭제
    
    * chore: 안쓰이는 의존성 삭제
    
    * style: 안 쓰이는 버전 삭제
    
    * style: 안 쓰이는 import 삭제
    
    * refactor: 버튼 컴포저블로 분리, 컴포즈 프리뷰 추가
    
    * style: 매니페스트 홈 액티비티 코드 제거함
    
    * feat: 카카오 키해시 가져오는 함수 추가
    
    ---------
    
    Co-authored-by: viva-hazel <oreocube13@gmail.com>
    Co-authored-by: AnKyungMoo <sodp2@naver.com>

--- app/src/main/java/com/mashup/alcoholfree/App.kt
+++ app/src/main/java/com/mashup/alcoholfree/App.kt
@@ -1,7 +1,17 @@
 package com.mashup.alcoholfree
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App: Application()
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        initKakaoSdk()
+    }
+
+    private fun initKakaoSdk() {
+        KakaoSdk.init(this, BuildConfig.KAKAO_KEY)
+    }
+}
