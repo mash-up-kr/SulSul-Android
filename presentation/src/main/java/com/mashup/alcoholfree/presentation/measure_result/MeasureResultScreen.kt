44645281dbfd43449d6398dfd4ef8daf2740e3fa
AnKyungMoo <sodp2@naver.com>
2023-06-06 16:41:27 +0900

    feat: #16 측정 상세정보 표시 컴포넌트 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/measure_result/MeasureResultScreen.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/measure_result/MeasureResultScreen.kt
@@ -1,27 +1,37 @@
 package com.mashup.alcoholfree.presentation.measure_result
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.mashup.alcoholfree.presentation.R
 import com.mashup.alcoholfree.presentation.ui.theme.Grey800
 import com.mashup.alcoholfree.presentation.ui.theme.Grey900
 import com.mashup.alcoholfree.presentation.ui.theme.H1
+import com.mashup.alcoholfree.presentation.ui.theme.H4
 import com.mashup.alcoholfree.presentation.ui.theme.H5
 import com.mashup.alcoholfree.presentation.ui.theme.SubPurple
 import com.mashup.alcoholfree.presentation.ui.theme.SubPurple16
 import com.mashup.alcoholfree.presentation.ui.theme.SubTitle3
+import com.mashup.alcoholfree.presentation.ui.theme.SubTitle4
 import com.mashup.alcoholfree.presentation.ui.theme.White
+import com.mashup.alcoholfree.presentation.ui.theme.White40
+
+private val rootHorizontalPadding = 16.dp
 
 @Composable
 fun MeasureResultScreen() {
@@ -36,6 +46,26 @@ fun MeasureResultScreen() {
         MeasureAlcoholCupCountBox(
             modifier = Modifier.padding(top = 8.dp),
             alcoholCupCount = 25,
+        )
+
+        MeasureResultInfoItems(
+            modifier = Modifier.padding(
+                top = 32.dp,
+                start = rootHorizontalPadding,
+                end = rootHorizontalPadding
+            ),
+            kcal = 132,
+            alcohol = 16.9f,
+            time = "3시간 20분",
+        )
+
+        Divider(
+            modifier = Modifier.padding(
+                top = 40.dp,
+                start = rootHorizontalPadding,
+                end = rootHorizontalPadding
+            ),
+            color = White40,
         )
     }
 }
@@ -69,6 +99,49 @@ fun MeasureAlcoholCupCountBox(modifier: Modifier = Modifier, alcoholCupCount: In
             style = SubTitle3,
             color = SubPurple,
         )
+    }
+}
+
+@Composable
+fun MeasureResultInfoItems(
+    modifier: Modifier = Modifier,
+    kcal: Int,
+    alcohol: Float,
+    time: String,
+) {
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        /* TODO: icon 변경 되어야함 */
+        MeasureResultInfoItem(
+            imageResId = R.drawable.icon_clock,
+            mainText = "$kcal Kcal",
+            subText = "오늘 마신 술 칼로리",
+        )
+        MeasureResultInfoItem(
+            imageResId = R.drawable.icon_clock,
+            mainText = "${alcohol}%",
+            subText = "평균 도수",
+        )
+        MeasureResultInfoItem(
+            imageResId = R.drawable.icon_clock,
+            mainText = time,
+            subText = "마신 시간",
+        )
+    }
+}
+
+@Composable
+fun MeasureResultInfoItem(
+    imageResId: Int,
+    mainText: String,
+    subText: String,
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(id = imageResId), contentDescription = null)
+        Text(text = mainText, style = H4, color = White)
+        Text(text = subText, style = SubTitle4, color = Grey900)
     }
 }
 
