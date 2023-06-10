95129a84216c5d45ba78906e41c12b4f2c7825b5
seojeongwoo <qw4643987@naver.com>
2023-06-11 03:33:12 +0900

    feat: #25 sulsul 공통 버튼 컬러 enum class 생성

---
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/component/model/SulSulButtonColor.kt
@@ -0,0 +1,16 @@
+package com.mashup.alcoholfree.presentation.ui.component.model
+
+import androidx.compose.ui.graphics.Color
+import com.mashup.alcoholfree.presentation.ui.theme.Grey200
+import com.mashup.alcoholfree.presentation.ui.theme.Grey300
+import com.mashup.alcoholfree.presentation.ui.theme.Primary100
+import com.mashup.alcoholfree.presentation.ui.theme.Primary200
+
+enum class SulSulButtonColor(
+    val color: Color,
+) {
+    PRIMARY100(Primary100),
+    PRIMARY200(Primary200),
+    GREY300(Grey300),
+    GREY200(Grey200),
+}
