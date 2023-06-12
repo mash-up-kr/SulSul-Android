608500c5322d560c3cc1adc564cb2e482aded81e
Jihye Joo <oreocube13@gmail.com>
2023-06-13 00:48:57 +0900

    feat: #33 선택된 주종 버블 컴포저블

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measuring/MeasuringActivity.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measuring/MeasuringActivity.kt
@@ -3,6 +3,7 @@ package com.mashup.alcoholfree.presentation.ui.measuring
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.mashup.alcoholfree.presentation.measure_result.model.AlcoholType
 
 class MeasuringActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,7 @@ class MeasuringActivity : ComponentActivity() {
                 totalCount = 25,
                 records = "와인 2잔 · 소주 2잔 · 맥주 3잔",
                 level = "미쳤다",
+                currentAlcoholType = AlcoholType.SOJU,
             )
         }
     }
--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measuring/MeasuringScreen.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measuring/MeasuringScreen.kt
@@ -1,5 +1,6 @@
 package com.mashup.alcoholfree.presentation.ui.measuring
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,10 +8,12 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mashup.alcoholfree.presentation.R
+import com.mashup.alcoholfree.presentation.measure_result.model.AlcoholType
 import com.mashup.alcoholfree.presentation.ui.component.SulSulLargeBadge
 import com.mashup.alcoholfree.presentation.ui.component.model.SulSulBadgeType
 import com.mashup.alcoholfree.presentation.ui.theme.AlcoholFreeAndroidTheme
@@ -23,6 +26,7 @@ fun MeasuringScreen(
     totalCount: Int,
     records: String,
     level: String,
+    currentAlcoholType: AlcoholType,
 ) {
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -36,11 +40,16 @@ fun MeasuringScreen(
 
         if (totalCount > 0) {
             SulSulLargeBadge(
-                modifier = Modifier.padding(vertical = 16.dp),
+                modifier = Modifier.padding(top = 16.dp),
                 type = SulSulBadgeType.PURPLE,
                 text = level
             )
         }
+
+        MeasuringSelectedAlcoholBubble(
+            modifier = Modifier.padding(vertical = 16.dp),
+            selectedAlcoholType = currentAlcoholType
+        )
     }
 }
 
@@ -54,7 +63,11 @@ private fun MeasuringHeader(
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text(text = stringResource(id = R.string.alcohol_cup_count, totalCount), style = H1, color = White)
+        Text(
+            text = stringResource(id = R.string.alcohol_cup_count, totalCount),
+            style = H1,
+            color = White
+        )
         Text(
             modifier = Modifier.padding(top = 4.dp),
             text = status,
@@ -62,6 +75,25 @@ private fun MeasuringHeader(
             color = White
         )
     }
+}
+
+@Composable
+private fun MeasuringSelectedAlcoholBubble(
+    modifier: Modifier = Modifier,
+    selectedAlcoholType: AlcoholType
+) {
+    val resId = when (selectedAlcoholType) {
+        AlcoholType.SOJU -> R.drawable.ic_bubble_soju
+        AlcoholType.BEER -> R.drawable.ic_bubble_macju
+        AlcoholType.KAOLIANGJU -> R.drawable.ic_bubble_goryanju
+        AlcoholType.WINE -> R.drawable.ic_bubble_wine
+    }
+
+    Image(
+        modifier = modifier,
+        painter = painterResource(id = resId),
+        contentDescription = null
+    )
 }
 
 @Preview
@@ -72,6 +104,7 @@ private fun MeasuringScreenPreview() {
             totalCount = 25,
             records = "와인 2잔 · 소주 2잔 · 맥주 3잔",
             level = "미쳤다",
+            currentAlcoholType = AlcoholType.SOJU,
         )
     }
 }
