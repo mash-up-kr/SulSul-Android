65e910246fbbdb8b5cfc3e052ca17abfe5ae222c
SEO-J17 <qw4643987@naver.com>
2023-07-19 01:21:33 +0900

    style: #73 누락된 그라디언트 컬러 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/theme/Color.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/theme/Color.kt
@@ -38,3 +38,8 @@ val SubPurpleTransparent16 = Color(0x29BBB6FF)
 val WhiteTransparent32 = Color(0x52FFFFFF)
 
 val EmptyGradient = Color(0x33747474)
+
+val SojuGradient = Color(0x336BD7D1)
+val WineGradient = Color(0xFF46265F)
+val BeerGradient = Color(0xFF4F3F17)
+val GoryangjuGradient = Color(0xFF364F17)
