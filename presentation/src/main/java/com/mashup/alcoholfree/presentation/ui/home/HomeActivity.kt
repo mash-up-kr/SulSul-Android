bba137138c3d4059ae50ce046b3db9add36c501d
SEO-J17 <qw4643987@naver.com>
2023-07-19 01:07:22 +0900

    feat: #29 cardState 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/HomeActivity.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/HomeActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.mashup.alcoholfree.presentation.ui.home.model.AlcoholLevel
+import com.mashup.alcoholfree.presentation.ui.home.model.AlcoholPromiseCardState
+import com.mashup.alcoholfree.presentation.ui.home.model.Drink
 import com.mashup.alcoholfree.presentation.ui.home.model.HomeState
 
 class HomeActivity : ComponentActivity() {
@@ -14,8 +16,16 @@ class HomeActivity : ComponentActivity() {
             HomeScreen(
                 HomeState(
                     userName = "우진",
-                    alcoholLevel = AlcoholLevel.LEVEL3
-                )
+                    alcoholLevel = AlcoholLevel.LEVEL3,
+                ),
+                AlcoholPromiseCardState(
+                    drinks = listOf(
+                        Drink("맥주", "1"),
+                        Drink("와인", "2"),
+                    ),
+                    drankDate = "2023-08-21T04:00:00Z",
+                    subTitleText = "술 좀 치네",
+                ),
             )
         }
     }
