0dbb7a67a2eb805088d87eb310e879ae88f4b909
SEO-J17 <qw4643987@naver.com>
2023-07-19 01:17:59 +0900

    style: #29 AlcoholPromiseCardType 이름 으로 코드 적용

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/model/AlcoholPromiseCardState.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/model/AlcoholPromiseCardState.kt
@@ -6,14 +6,14 @@ data class AlcoholPromiseCardState(
     val subTitleText: String,
 ) {
     companion object {
-        fun getAlcoholType(type: Drink): AlcoholCardType {
+        fun getAlcoholType(type: Drink): AlcoholPromiseCardType {
             return when (type.alcoholType) {
-                "소주" -> AlcoholCardType.SOJU
-                "와인" -> AlcoholCardType.WINE
-                "맥주" -> AlcoholCardType.BEER
-                "고량주" -> AlcoholCardType.GORYANGJU
+                "소주" -> AlcoholPromiseCardType.SOJU
+                "와인" -> AlcoholPromiseCardType.WINE
+                "맥주" -> AlcoholPromiseCardType.BEER
+                "고량주" -> AlcoholPromiseCardType.GORYANGJU
                 // TODO: 술을 안마셨다면 나중에 빈 컴포넌트 색깔 넣으면 될듯 합니다~
-                else -> AlcoholCardType.SOJU
+                else -> AlcoholPromiseCardType.SOJU
             }
         }
     }
