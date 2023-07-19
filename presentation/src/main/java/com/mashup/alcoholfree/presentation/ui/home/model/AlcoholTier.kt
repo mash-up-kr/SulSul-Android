6430eb59eb3dcb5b8b27310f1fb157d494adc1c7
viva-hazel <oreocube13@gmail.com>
2023-07-19 22:48:09 +0900

    #66 [홈] 칭호 노데이터 컴포넌트 (#68)
    * feat: #66 - 칭호 없을 때 보일 컴포넌트 레이아웃
    
    * refactor: #66 - 스트링 리소스 분리
    
    * chore: #66 - AlcoholLevelCard 내부로 이동
    
    * chore: #66 - AlcoholLevelCard -> AlcoholTierCard 네이밍 변경
    
    * chore: #66 - Level -> Tier 네이밍 변경
    
    * feat: #66 - 기본 칭호 카드에도 그레인 백그라운드 적용

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/model/AlcoholTier.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/model/AlcoholTier.kt
@@ -2,7 +2,7 @@ package com.mashup.alcoholfree.presentation.ui.home.model
 
 import com.mashup.alcoholfree.presentation.R
 
-enum class AlcoholLevel(val title: String, val badgeText: String, val imageResId: Int) {
+enum class AlcoholTier(val title: String, val badgeText: String, val imageResId: Int) {
     LEVEL1(title = "응애 술요미!", badgeText = "귀엽네", imageResId = R.drawable.img_baby),
     LEVEL2(title = "그저 술반인", badgeText = "가자~", imageResId = R.drawable.img_common),
     LEVEL3(title = "이쯤되면 술잘알", badgeText = "술 좀 치네", imageResId = R.drawable.img_master),
