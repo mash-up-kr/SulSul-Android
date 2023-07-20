4dc5e7d395dc75479fa6f33b7daf60f061b9906f
SEO-J17 <qw4643987@naver.com>
2023-07-20 22:53:20 +0900

    style: #29 필요없는 import 제거

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/HomeScreen.kt
@@ -2,13 +2,8 @@ package com.mashup.alcoholfree.presentation.ui.home
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
