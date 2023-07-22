a58e3db7fbd22027bf7697c017cde39709040399
SEO-J17 <qw4643987@naver.com>
2023-07-22 17:19:37 +0900

    feat: #60 주량 측정 결과 데이터 모델 생성

---
+++ data/src/main/java/com/mashup/alcoholfree/data/model/measure/MeasureResultDataModel.kt
@@ -0,0 +1,40 @@
+package com.mashup.alcoholfree.data.model.measure
+
+import com.mashup.alcoholfree.data.remote.response.MeasureResultResponse
+import com.mashup.alcoholfree.domain.model.MeasureResult
+
+data class MeasureResultDataModel(
+    val alcoholCalorie: Int,
+    val averageAlcoholContent: Double,
+    val drankAt: String,
+    val drinkingDuration: String,
+    val drinks: List<DrinkDataModel>,
+    val id: String,
+    val totalDrinkGlasses: Int,
+) {
+    companion object {
+        operator fun invoke(response: MeasureResultResponse): MeasureResultDataModel {
+            return MeasureResultDataModel(
+                alcoholCalorie = response.alcoholCalorie,
+                averageAlcoholContent = response.averageAlcoholContent,
+                drankAt = response.drankAt,
+                drinkingDuration = response.drinkingDuration,
+                drinks = response.drinks.map { DrinkDataModel(it) },
+                id = response.id,
+                totalDrinkGlasses = response.totalDrinkGlasses,
+            )
+        }
+
+        fun toDomainModel(data: MeasureResultDataModel): MeasureResult {
+            return MeasureResult(
+                alcoholCalorie = data.alcoholCalorie,
+                averageAlcoholContent = data.averageAlcoholContent,
+                drankAt = data.drankAt,
+                drinkingDuration = data.drinkingDuration,
+                drinks = data.drinks.map { DrinkDataModel.toDomainModel(it) },
+                id = data.id,
+                totalDrinkGlasses = data.totalDrinkGlasses,
+            )
+        }
+    }
+}
