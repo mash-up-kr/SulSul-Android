2c671a198265eb657a549d0024945f064aa26e28
SEO-J17 <qw4643987@naver.com>
2023-07-22 17:18:15 +0900

    feat: #60 측정 관련 네트워크 통신 구현, 인터셉터 추가

--- data/src/main/java/com/mashup/alcoholfree/data/di/NetworkModule.kt
+++ data/src/main/java/com/mashup/alcoholfree/data/di/NetworkModule.kt
@@ -1,38 +1,91 @@
 package com.mashup.alcoholfree.data.di
 
-import com.mashup.alcoholfree.data.service.SulSulService
+import com.mashup.alcoholfree.data.BuildConfig
+import com.mashup.alcoholfree.data.TimeOutPolicy
+import com.mashup.alcoholfree.data.datasource.LocalLoginDataSource
+import com.mashup.alcoholfree.data.remote.measure.MeasureService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 internal class NetworkModule {
+
+    @Singleton
+    @Provides
+    @TimeOutPolicy
+    fun provideConnectTimeoutPolicy(): Long {
+        return 10_000L
+    }
+
+    @Singleton
+    @Provides
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Singleton
+    @Provides
+    fun provideAuthInterceptor(loginDataSource: LocalLoginDataSource): Interceptor {
+        return Interceptor { chain ->
+            val authToken = runBlocking {
+                loginDataSource.getToken().first()
+            }
+            chain.proceed(
+                chain.request()
+                    .newBuilder()
+                    .addHeader("Authorization", "Bearer $authToken")
+                    .build(),
+            )
+        }
+    }
+
     @Provides
     @Singleton
-    fun provideSulSulService(retrofit: Retrofit): SulSulService =
-        retrofit.create(SulSulService::class.java)
+    fun provideOkHttpClient(
+        authInterceptor: Interceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+        @TimeOutPolicy
+        connectTimeoutPolicy: Long,
+    ): OkHttpClient =
+        OkHttpClient
+            .Builder()
+            .addInterceptor(authInterceptor)
+            .addInterceptor(httpLoggingInterceptor)
+            .connectTimeout(
+                connectTimeoutPolicy,
+                TimeUnit.MILLISECONDS,
+            )
+            .build()
 
     @Provides
     @Singleton
     fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
         Retrofit.Builder()
-            .baseUrl("https://sulsul.app/")
+            .baseUrl("https://sulsul.app")
             .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient =
-        OkHttpClient
-            .Builder()
-            .addInterceptor(HttpLoggingInterceptor())
-            .build()
+    fun provideMeasureService(retrofit: Retrofit): MeasureService =
+        retrofit.create(MeasureService::class.java)
 }
