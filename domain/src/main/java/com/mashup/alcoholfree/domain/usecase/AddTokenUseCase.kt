d7d494e3c25f65e378af72bf32b446a33beaf53b
SEO-J17 <qw4643987@naver.com>
2023-07-23 00:00:11 +0900

    style: #60 패키지 이동

--- domain/src/main/java/com/mashup/alcoholfree/domain/usecase/AddTokenUseCase.kt
+++ domain/src/main/java/com/mashup/alcoholfree/domain/usecase/AddTokenUseCase.kt
@@ -1,6 +1,6 @@
-package com.mashup.alcoholfree.domain.usecase.login
+package com.mashup.alcoholfree.domain.usecase
 
-import com.mashup.alcoholfree.domain.repository.login.LoginRepository
+import com.mashup.alcoholfree.domain.repository.LoginRepository
 import dagger.Reusable
 import javax.inject.Inject
 
