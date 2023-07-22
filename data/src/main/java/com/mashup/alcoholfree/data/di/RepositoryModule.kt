a3f79e58cb92f44cb9c7f6afa06b8965fd191411
SEO-J17 <qw4643987@naver.com>
2023-07-22 17:17:10 +0900

    feat: #60 측정 관련 리포지토리 bind 코드 추가

--- data/src/main/java/com/mashup/alcoholfree/data/di/RepositoryModule.kt
+++ data/src/main/java/com/mashup/alcoholfree/data/di/RepositoryModule.kt
@@ -1,7 +1,9 @@
 package com.mashup.alcoholfree.data.di
 
-import com.mashup.alcoholfree.data.repository.LoginRepositoryImpl
-import com.mashup.alcoholfree.domain.repository.LoginRepository
+import com.mashup.alcoholfree.data.repository.login.LoginRepositoryImpl
+import com.mashup.alcoholfree.data.repository.measure.MeasureRepositoryImpl
+import com.mashup.alcoholfree.domain.repository.login.LoginRepository
+import com.mashup.alcoholfree.domain.repository.measure.MeasureRepository
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -13,4 +15,7 @@ abstract class RepositoryModule {
 
     @Binds
     abstract fun bindLoginRepository(loginRepositoryImpl: LoginRepositoryImpl): LoginRepository
+
+    @Binds
+    abstract fun bindMeasureRepository(measureRepositoryImpl: MeasureRepositoryImpl): MeasureRepository
 }
