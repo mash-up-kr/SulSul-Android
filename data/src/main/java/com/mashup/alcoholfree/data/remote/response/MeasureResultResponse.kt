ee0f51db75439d600d0101ab8b5d1e18bf286d6f
SEO-J17 <qw4643987@naver.com>
2023-07-22 17:19:31 +0900

    feat: #60 주량 측정 결과 리스폰스 모델 생성

---
+++ data/src/main/java/com/mashup/alcoholfree/data/remote/response/MeasureResultResponse.kt
@@ -0,0 +1,20 @@
+package com.mashup.alcoholfree.data.remote.response
+
+import com.google.gson.annotations.SerializedName
+
+data class MeasureResultResponse(
+    @SerializedName("alcoholCalorie")
+    val alcoholCalorie: Int,
+    @SerializedName("averageAlcoholContent")
+    val averageAlcoholContent: Double,
+    @SerializedName("drankAt")
+    val drankAt: String,
+    @SerializedName("drinkingDuration")
+    val drinkingDuration: String,
+    @SerializedName("drinks")
+    val drinks: List<DrinkResponse>,
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("totalDrinkGlasses")
+    val totalDrinkGlasses: Int,
+)
