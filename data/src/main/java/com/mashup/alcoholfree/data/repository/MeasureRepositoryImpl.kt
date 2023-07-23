8e6803da63c8a22a78ffa6c10a45bdccfd1450da
seojeongwoo <qw4643987@naver.com>
2023-07-23 17:02:04 +0900

    style: #60 수정한 코드 적용

--- data/src/main/java/com/mashup/alcoholfree/data/repository/MeasureRepositoryImpl.kt
+++ data/src/main/java/com/mashup/alcoholfree/data/repository/MeasureRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.mashup.alcoholfree.data.repository
 
 import com.mashup.alcoholfree.data.datasource.MeasureDataSource
-import com.mashup.alcoholfree.data.dto.remote.response.MeasureResultResponse
 import com.mashup.alcoholfree.domain.model.MeasureResult
 import com.mashup.alcoholfree.domain.repository.MeasureRepository
 import javax.inject.Inject
@@ -10,8 +9,8 @@ class MeasureRepositoryImpl @Inject constructor(
     private val measureDataSource: MeasureDataSource,
 ) : MeasureRepository {
     override suspend fun getMeasureResult(reportId: String): MeasureResult {
-        return MeasureResultResponse.toDomainModel(
-            measureDataSource.getMeasureResult(reportId),
-        )
+        return measureDataSource
+            .getMeasureResult(reportId)
+            .toDomainModel()
     }
 }
