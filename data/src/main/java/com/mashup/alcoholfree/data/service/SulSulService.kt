e44c5018c9610cf0a5fed9df9759e769f391f19c
SEO-J17 <qw4643987@naver.com>
2023-07-25 00:02:40 +0900

    feat: #59 주량 측정 결과 리포트 서비스 구현

--- data/src/main/java/com/mashup/alcoholfree/data/service/SulSulService.kt
+++ data/src/main/java/com/mashup/alcoholfree/data/service/SulSulService.kt
@@ -1,16 +1,28 @@
 package com.mashup.alcoholfree.data.service
 
+import com.mashup.alcoholfree.data.dto.remote.request.MeasureResultReportRequest
+import com.mashup.alcoholfree.data.dto.remote.response.MeasureResultReportResponse
 import com.mashup.alcoholfree.data.dto.remote.response.MeasureResultResponse
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface SulSulService {
     /*
      * 측정 결과 리포트 조회
      */
-    @GET("/api/v1/drinkingReport/")
+    @GET("/api/v1/drinkingReport")
     fun getMeasuringResult(
         @Query("reportId") reportId: String,
     ): Call<MeasureResultResponse>
+
+    /**
+     * 측정 결과 리포트 생성
+     */
+    @POST("/api/v1/drinkingReport")
+    fun requestMeasureResultReport(
+        @Body resultReport: MeasureResultReportRequest,
+    ): Call<MeasureResultReportResponse>
 }
