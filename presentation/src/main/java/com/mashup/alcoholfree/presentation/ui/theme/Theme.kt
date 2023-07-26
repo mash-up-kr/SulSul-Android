dc030fa371af2189afc93d215b31be012e235f30
Jihye Joo <oreocube13@gmail.com>
2023-07-26 23:35:35 +0900

    feat: #81 - 기본 테마 status bar 투명하게 설정

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/theme/Theme.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/theme/Theme.kt
@@ -5,6 +5,9 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.Color
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -36,6 +39,11 @@ fun AlcoholFreeAndroidTheme(
         DarkColorPalette
     } else {
         LightColorPalette
+    }
+
+    val systemUiController = rememberSystemUiController()
+    SideEffect {
+        systemUiController.setSystemBarsColor(Color.Transparent)
     }
 
     MaterialTheme(
