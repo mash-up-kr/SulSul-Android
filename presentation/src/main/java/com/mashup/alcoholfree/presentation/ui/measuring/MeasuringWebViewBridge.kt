8126532a720515ce960032d5cf0367b6cec39ea0
SEO-J17 <qw4643987@naver.com>
2023-07-27 01:39:17 +0900

    feat: #87 웹뷰 브릿지 생성

---
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measuring/MeasuringWebViewBridge.kt
@@ -0,0 +1,34 @@
+package com.mashup.alcoholfree.presentation.ui.measuring
+
+import android.webkit.JavascriptInterface
+import android.webkit.WebView
+import com.mashup.alcoholfree.presentation.constants.WebViewConstant
+
+class MeasuringWebViewBridge(
+    private val webView: WebView,
+) {
+
+    @JavascriptInterface
+    fun publishEvent(
+        functionName: String,
+        alcoholType: String,
+    ): String {
+        val buffer = StringBuffer()
+            .append("window.dispatchEvent(\n")
+            .append("   new CustomEvent(\"").append(functionName).append("\", {\n")
+            .append("           detail: {\n")
+            .append("               data: ").append(alcoholType).append("\n")
+            .append("           }\n")
+            .append("       }\n")
+            .append("   )\n")
+            .append(");")
+        return buffer.toString()
+    }
+
+    @JavascriptInterface
+    fun addBallClick(alcoholType: String) {
+        webView.evaluateJavascript(
+            publishEvent(WebViewConstant.WEB_FUNCTION_NAME, alcoholType),
+        ) {}
+    }
+}
