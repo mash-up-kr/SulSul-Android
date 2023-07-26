03d31a8cb7adec6747535b003ae24a21339a02a3
Jihye Joo <oreocube13@gmail.com>
2023-07-26 23:47:01 +0900

    feat: #81 - status bar padding 적용

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measureresult/MeasureResultScreen.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measureresult/MeasureResultScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -124,7 +125,9 @@ fun MeasureResultScreen(
 @Composable
 private fun MeasureResultContent(state: MeasureResultState) {
     Column(
-        modifier = Modifier.verticalScroll(rememberScrollState()),
+        modifier = Modifier
+            .verticalScroll(rememberScrollState())
+            .statusBarsPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
         MeasureResultHeader(
