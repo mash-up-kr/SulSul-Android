3c8a7936a87d4765d518f77cff5595a7ce455d00
SEO-J17 <qw4643987@naver.com>
2023-07-27 01:42:24 +0900

    feat: #87 웹뷰와 브릿지 연결, 클릭시 브릿지 함수 실행 하는 코드 구현

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/component/WebView.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/component/WebView.kt
@@ -1,42 +1,69 @@
 package com.mashup.alcoholfree.presentation.ui.component
 
 import android.annotation.SuppressLint
+import android.view.HapticFeedbackConstants
+import android.view.MotionEvent
+import android.webkit.WebView
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.WebViewState
+import com.mashup.alcoholfree.presentation.constants.WebViewConstant
+import com.mashup.alcoholfree.presentation.ui.measuring.MeasuringWebViewBridge
 
-@Composable
-fun SulSulWebView(
-    modifier: Modifier = Modifier,
-    state: WebViewState,
-) {
-    WebView(
-        modifier = modifier,
-        state = state,
-        onCreated = { webView ->
-            webView.setBackgroundColor(0) // 웹뷰 투명 배경
-        }
-    )
-}
+private lateinit var currentAlcohol: String
 
 @SuppressLint("SetJavaScriptEnabled", "JavascriptInterface")
 @Composable
 fun SulSulWebView(
     modifier: Modifier = Modifier,
     state: WebViewState,
-    bridge: Any,
-    bridgeName: String,
+    currentAlcoholType: String,
+) {
+    currentAlcohol = currentAlcoholType
+
+    WebView(
+        modifier = modifier,
+        state = state,
+        onCreated = { webView ->
+            val webViewBridge = MeasuringWebViewBridge(webView)
+            with(webView) {
+                setBackgroundColor(0) // 웹뷰 투명 배경
+                settings.javaScriptEnabled = true
+                addJavascriptInterface(webViewBridge, WebViewConstant.BRIDGE_NAME)
+                onWebViewTouch(webView, webViewBridge)
+            }
+        },
+    )
+}
+
+@SuppressLint("SetJavaScriptEnabled")
+@Composable
+fun SulSulWebView(
+    modifier: Modifier = Modifier,
+    state: WebViewState,
 ) {
     WebView(
         modifier = modifier,
         state = state,
         onCreated = { webView ->
             with(webView) {
-                setBackgroundColor(0) // 웹뷰 투명 배경
                 settings.javaScriptEnabled = true
-                addJavascriptInterface(bridge, bridgeName)
             }
-        }
+        },
     )
+}
+
+@SuppressLint("ClickableViewAccessibility")
+private fun onWebViewTouch(webView: WebView, webViewBridge: MeasuringWebViewBridge) {
+    webView.setOnTouchListener { view, motionEvent ->
+        if (
+            view.id == webView.id &&
+            motionEvent.action == MotionEvent.ACTION_DOWN
+        ) {
+            view.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+            webViewBridge.addBallClickListener(currentAlcohol)
+        }
+        false
+    }
 }
