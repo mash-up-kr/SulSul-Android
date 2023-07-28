4d41046adb8ed2b617f977286ef394982b7f9998
SEO-J17 <qw4643987@naver.com>
2023-07-29 00:35:25 +0900

    feat: #95 액티비티 익스텐션에 번들 인자 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/utils/ActivityKtx.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/utils/ActivityKtx.kt
@@ -2,9 +2,15 @@ package com.mashup.alcoholfree.presentation.utils
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Bundle
 
-fun Activity.moveToActivity(activity: Class<*>) {
+fun Activity.moveToActivity(
+    activity: Class<*>,
+    bundleOf: Bundle? = null,
+) {
     startActivity(
-        Intent(this, activity),
+        Intent(this, activity).apply {
+            bundleOf?.let { putExtras(it) }
+        },
     )
 }
