e2e1393bb74a9bde8a9ed8c956b7d31fdb6343ef
seojeongwoo <qw4643987@naver.com>
2023-07-29 15:13:43 +0900

    feat: 이동하는 함수 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/HomeActivity.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/home/HomeActivity.kt
@@ -5,13 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.runtime.getValue
-import androidx.core.os.bundleOf
 import androidx.core.view.WindowCompat
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.mashup.alcoholfree.presentation.ui.measureresult.MeasureResultActivity
 import com.mashup.alcoholfree.presentation.ui.measuring.MeasuringActivity
 import com.mashup.alcoholfree.presentation.ui.theme.AlcoholFreeAndroidTheme
-import com.mashup.alcoholfree.presentation.utils.moveToActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -30,13 +28,8 @@ class HomeActivity : ComponentActivity() {
 
                 HomeScreen(
                     state = state,
-                    onAlcoholCardClick = {
-                        moveToActivity(
-                            MeasureResultActivity::class.java,
-                            bundleOf("reportId" to it),
-                        )
-                    },
-                    onDrinkAlcoholClick = { moveToActivity(MeasuringActivity::class.java) },
+                    onAlcoholCardClick = { navigateToMeasureResult(reportId = REPORT_ID) },
+                    onDrinkAlcoholClick = { navigateToMeasure() },
                 )
             }
         }
@@ -45,5 +38,17 @@ class HomeActivity : ComponentActivity() {
     private fun initData() = with(viewModel) {
         getUserInfo()
         getAlcoholPromiseCards()
+    }
+
+    private fun navigateToMeasure() {
+        startActivity(MeasuringActivity.getIntent(this))
+    }
+
+    private fun navigateToMeasureResult(reportId: String) {
+        startActivity(MeasureResultActivity.getIntent(this, reportId))
+    }
+
+    companion object {
+        private const val REPORT_ID = "reportId"
     }
 }
