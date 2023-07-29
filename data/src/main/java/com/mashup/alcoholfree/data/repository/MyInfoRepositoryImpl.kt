0985b75e059f7fe460fa1c2f59af6cb6e0db2f83
seojeongwoo <qw4643987@naver.com>
2023-07-30 02:21:17 +0900

    fix: #103 리포지토리에서 inject 서비스에서 데이터 소스로 수정

--- data/src/main/java/com/mashup/alcoholfree/data/repository/MyInfoRepositoryImpl.kt
+++ data/src/main/java/com/mashup/alcoholfree/data/repository/MyInfoRepositoryImpl.kt
@@ -1,18 +1,16 @@
 package com.mashup.alcoholfree.data.repository
 
-import com.mashup.alcoholfree.data.service.SulSulService
+import com.mashup.alcoholfree.data.datasource.MyInfoDataSource
 import com.mashup.alcoholfree.domain.model.MyInfo
 import com.mashup.alcoholfree.domain.repository.MyInfoRepository
-import retrofit2.await
 import javax.inject.Inject
 
 class MyInfoRepositoryImpl @Inject constructor(
-    private val sulService: SulSulService,
+    private val myInfoDataSource: MyInfoDataSource,
 ) : MyInfoRepository {
     override suspend fun getMyInfo(): MyInfo {
-        return sulService
+        return myInfoDataSource
             .getMyInfo()
-            .await()
             .toDomainModel()
     }
 }
