8d5b9960a264b2f6910ddcbb06d6b1071fd1840e
seojeongwoo <qw4643987@naver.com>
2023-07-29 17:24:48 +0900

    remove: 필요없는 파일 삭제

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/constants/WebViewConstant.kt
+++
@@ -1,8 +0,0 @@
-package com.mashup.alcoholfree.presentation.constants
-
-object WebViewConstant {
-    const val WEB_MEASURE_URL = "https://dev-onboarding.sulsul.app/measure"
-    const val WEB_FALLING_URL = "https://dev-falling.sulsul.app"
-    const val BRIDGE_NAME = "sulsulBridge"
-    const val WEB_FUNCTION_NAME = "addBall"
-}
