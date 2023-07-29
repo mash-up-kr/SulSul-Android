b57d0ccf8f0d3b82443a16b28b456098caacf2ba
seojeongwoo <qw4643987@naver.com>
2023-07-30 00:55:17 +0900

    feat: #106 백 버튼 클릭시 다이얼로그 생성

---
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/measuring/AlcoholBackPressDialog.kt
@@ -0,0 +1,103 @@
+package com.mashup.alcoholfree.presentation.ui.measuring
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.mashup.alcoholfree.presentation.R
+import com.mashup.alcoholfree.presentation.ui.component.SulSulDefaultButton
+import com.mashup.alcoholfree.presentation.ui.component.model.SulSulButtonColor
+import com.mashup.alcoholfree.presentation.ui.component.model.SulSulButtonSize
+import com.mashup.alcoholfree.presentation.ui.theme.Grey000
+import com.mashup.alcoholfree.presentation.ui.theme.Grey700
+import com.mashup.alcoholfree.presentation.ui.theme.Grey900
+import com.mashup.alcoholfree.presentation.ui.theme.H3
+import com.mashup.alcoholfree.presentation.ui.theme.SubTitle2
+
+@Composable
+fun AlcoholBackPressDialog(
+    onContinueClick: () -> Unit,
+    onExitClick: () -> Unit,
+) {
+    Dialog(onDismissRequest = onContinueClick) {
+        Column(
+            modifier = Modifier
+                .clip(shape = RoundedCornerShape(16.dp))
+                .background(color = Grey000)
+                .padding(horizontal = 16.dp, vertical = 24.dp),
+        ) {
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = stringResource(R.string.back_press_exit_label),
+                style = H3,
+                color = Grey900,
+            )
+
+            Text(
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 4.dp),
+                text = stringResource(R.string.back_press_dialog_content),
+                style = SubTitle2,
+                color = Grey700,
+            )
+
+            DialogButtonGroup(
+                modifier = Modifier
+                    .padding(top = 16.dp),
+                onEnterClick = onContinueClick,
+                onExitClick = onExitClick,
+            )
+        }
+    }
+}
+
+@Composable
+private fun DialogButtonGroup(
+    modifier: Modifier = Modifier,
+    onEnterClick: () -> Unit,
+    onExitClick: () -> Unit,
+) {
+    Row(
+        modifier = modifier,
+    ) {
+        SulSulDefaultButton(
+            modifier = Modifier.weight(1f),
+            content = stringResource(R.string.exit_dialog_keep_label),
+            buttonColor = SulSulButtonColor.GREY300,
+            buttonSize = SulSulButtonSize.LARGE,
+            onClick = onEnterClick,
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        SulSulDefaultButton(
+            modifier = Modifier.weight(1f),
+            content = stringResource(R.string.exit_dialog_finish_label),
+            buttonColor = SulSulButtonColor.PRIMARY100,
+            buttonSize = SulSulButtonSize.LARGE,
+            onClick = onExitClick,
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun AlcoholBackPressDialogPreview() {
+    AlcoholBackPressDialog(
+        onExitClick = {},
+        onContinueClick = {},
+    )
+}
