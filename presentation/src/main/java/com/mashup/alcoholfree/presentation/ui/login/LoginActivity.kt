80f236af9c42f8d8d66f3127a791f510f250d4e5
SEO-J17 <qw4643987@naver.com>
2023-08-02 23:30:43 +0900

    feat: #138 로그인 성공시 홈화면으로 이동하는 로직 추가

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/login/LoginActivity.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/login/LoginActivity.kt
@@ -1,8 +1,8 @@
 package com.mashup.alcoholfree.presentation.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -15,6 +15,7 @@ import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
+import com.mashup.alcoholfree.presentation.ui.home.HomeActivity
 import com.mashup.alcoholfree.presentation.ui.theme.AlcoholFreeAndroidTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -66,24 +67,26 @@ class LoginActivity : ComponentActivity() {
     private fun kakaoAccountLogin() {
         UserApiClient.instance.loginWithKakaoAccount(this) { token, error ->
             if (error != null) {
-                Toast.makeText(
-                    this,
-                    " 로그인 실패",
-                    Toast.LENGTH_SHORT,
-                ).show()
+                Log.d("Kakao :", "로그인 실패")
             } else if (token != null) {
-                Toast.makeText(
-                    this,
-                    " 로그인 성공",
-                    Toast.LENGTH_SHORT,
-                ).show()
-                Log.d("Kakao Token : ", token.accessToken)
+                Log.d("Kakao : ", "로그인 성공")
                 viewModel.addKakaoToken(token)
+                navigateToHome()
             }
         }
     }
 
     private fun getKakaoKeyHash(): String {
         return Utility.getKeyHash(this)
+    }
+
+    private fun navigateToHome() {
+        startActivity(
+            Intent(
+                this,
+                HomeActivity::class.java,
+            ),
+        )
+        finish()
     }
 }
