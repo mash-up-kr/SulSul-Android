a1a9eb70f3a1958e21091e0de44a6603183fb15c
SEO-J17 <qw4643987@naver.com>
2023-08-04 18:08:43 +0900

    remove: #123 필요없는 코드 삭제

--- presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/splash/SplashActivity.kt
+++ presentation/src/main/java/com/mashup/alcoholfree/presentation/ui/splash/SplashActivity.kt
@@ -4,10 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import com.kakao.sdk.auth.AuthApiClient
@@ -24,12 +20,7 @@ class SplashActivity : ComponentActivity() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
             AlcoholFreeAndroidTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
-                ) {
-                    SplashScreen()
-                }
+                SplashScreen()
             }
         }
 
